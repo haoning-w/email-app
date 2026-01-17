2a231d01ea991ada82dbbbd39556636fa939f119
haoning-w <elwin.hn.wang@gmail.com>
2026-01-16 17:07:03 -0800

    Bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import EmailList from "@/components/ThreadList";
-import { FilterProvider } from "@/context/FilterContext";
 
 export default function Home() {
   return (
-    <FilterProvider>
-      <div className="h-screen flex flex-col bg-white dark:bg-gray-900">
-        <Header />
-        <div className="flex flex-1 overflow-hidden">
-          <Sidebar />
-          <EmailList />
-        </div>
+    <div className="h-screen flex flex-col bg-white dark:bg-gray-900">
+      <Header />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <EmailList />
       </div>
-    </FilterProvider>
+    </div>
   );
 }
